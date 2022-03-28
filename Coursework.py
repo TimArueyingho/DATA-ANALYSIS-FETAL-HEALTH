6151331456453f4215fbfe8465359465eb7f4c70
Tim Arueyingho <92372663+TheRealSheBoss@users.noreply.github.com>
2022-03-28 19:59:16 +0100

    Update Coursework.py

--- Coursework.py
+++ Coursework.py
@@ -2,9 +2,3 @@ import pandas as pd
 import numpy as np
 
 
-fetal_health = pd.read_csv("C:/Users/sj21399/OneDrive - University of Bristol/Desktop/DATA ANALYTICS CW/fetal_health.csv")
-fetal_health
-
-ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-hello rohin
