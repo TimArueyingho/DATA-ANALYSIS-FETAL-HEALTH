a5302ce83584fc6e07fd29997eb32b4015c153d9
Tim_Arueyingho <tim.arueyingho@bristol.ac.uk>
2022-03-28 20:01:33 +0100

    Exploratory data analysis

--- Coursework.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-
-fetal_health = pd.read_csv("C:/Users/sj21399/OneDrive - University of Bristol/Desktop/DATA ANALYTICS CW/fetal_health.csv")
-fetal_health
-
-
