38f0d533b70db5a143fb5c2dbf75033e463b2755
aekabris <sg21494@bristol.ac.uk>
2022-05-19 23:42:44 +0100

    Added loop to generate graphs for additional best features from DT and RF based feature selection Added Tim's hyperparam tuning for RF

--- Coursework.py
+++ Coursework.py
@@ -122,7 +122,7 @@ plt.show()
 #Unbalanced testing
 
 def model_tests(X_train, X_test,y_train,y_test):
-    models = {"KNeighborsClassifier":KNeighborsClassifier(), "GaussianNaiveBayes": GaussianNB(), "DecisionTree": DecisionTreeClassifier(random_state=20), "RandomForest": RandomForestClassifier(random_state=20)}
+    models = {"KNeighborsClassifier":KNeighborsClassifier(), "GaussianNaiveBayes": GaussianNB(), "DecisionTree": DecisionTreeClassifier(random_state=10), "RandomForest": RandomForestClassifier(random_state=10)}
     for named, model in models.items():
         mod = model.fit(X_train, y_train)
         ypred = mod.predict(X_test)
@@ -243,7 +243,7 @@ plt.show()
 # FEATURE SELECTION
 
 # In[66]
-models = {"KNeighborsClassifier":KNeighborsClassifier(), "GaussianNaiveBayes": GaussianNB(), "RandomForestClassifier": RandomForestClassifier()}
+models = {"KNeighborsClassifier":KNeighborsClassifier(), "GaussianNaiveBayes": GaussianNB(), "RandomForestClassifier": RandomForestClassifier(random_state=10)}
 for named, model in models.items(): 
     accuracyscores = []
     precisionscores = []
@@ -264,19 +264,17 @@ for named, model in models.items():
 
     #generating scores 
         accuracyscores.append(accuracy_score(y_test, ypred))
-        precisionscores.append(precision_score(y_test, ypred, average = 'weighted'))
-        recallscores.append(recall_score(y_test, ypred, average = 'weighted'))
-        f1scores.append(f1_score(y_test, ypred, average="weighted"))
-        
-        print(f"Results for {named} using {i} features ")
-        model_tests(Xtraintemp, Xtesttemp, y_train, ypred)
-        
+        precisionscores.append(precision_score(y_test, ypred, average = 'macro'))
+        recallscores.append(recall_score(y_test, ypred, average = 'macro'))
+        f1scores.append(f1_score(y_test, ypred, average="macro"))
+        print(f"For {model} with {i} features, selected features are: {sfs.get_feature_names_out()}")
+    print(f"\n")
     plt.figure()
     x = list(range(5,21))
-    plt.plot(x,accuracyscores, label = 'accuracy scores')
-    plt.plot(x,precisionscores, label = 'precision scores')
-    plt.plot(x, recallscores, label = 'recall scores')
-    plt.plot(x, f1scores, label = 'F1 scores')
+    plt.plot(x,accuracyscores, label = 'accuracy scores', color="green")
+    plt.plot(x,precisionscores, label = 'precision scores', color="blue")
+    plt.plot(x, recallscores, label = 'recall scores',color="red")
+    plt.plot(x, f1scores, label = 'F1 scores',color="purple")
     plt.xlabel('number of features used')
     plt.xticks(x)
     plt.title(f'Changes features used in {named}')
@@ -285,7 +283,7 @@ for named, model in models.items():
 # SECOND FEATURE SELECTION - trees
 import sklearn.tree as skt
 
-model = DecisionTreeClassifier()
+model = DecisionTreeClassifier(random_state=33)
 x = model.fit(X_train, y_train)
 plt.figure(figsize = (60,20))
 new = skt.plot_tree(x, filled=True)
@@ -308,6 +306,41 @@ plt.show()
 
 #Data not scaled as scaled data performed worse except for KNN
 
+models = {"KNeighborsClassifier":KNeighborsClassifier(), "GaussianNaiveBayes": GaussianNB(), "RandomForestClassifier": RandomForestClassifier(random_state=10)}
+for named, model in models.items(): 
+   
+    accuracyscores = []
+    precisionscores = []
+    recallscores= []
+    f1scores = []
+    
+    for i in range(len(dictionary.keys())):
+        feature_names = list(dictionary.keys())[:i+1]
+        best_X = X[feature_names]  
+
+        best_X_train, best_X_test, y_train, y_test = train_test_split(best_X, y, test_size=0.2, random_state = 40)
+        
+        model.fit(best_X_train, y_train)
+        ypred = model.predict(best_X_test)
+
+    #generating scores 
+        accuracyscores.append(accuracy_score(y_test, ypred))
+        precisionscores.append(precision_score(y_test, ypred, average = 'macro'))
+        recallscores.append(recall_score(y_test, ypred, average = 'macro'))
+        f1scores.append(f1_score(y_test, ypred, average="macro"))
+    
+    plt.figure()
+    x = range(1, len(dictionary.keys())+1)
+    plt.plot(x,accuracyscores, label = 'accuracy scores', color="green")
+    plt.plot(x,precisionscores, label = 'precision scores', color="blue")
+    plt.plot(x, recallscores, label = 'recall scores',color="red")
+    plt.plot(x, f1scores, label = 'F1 scores',color="purple")
+    plt.xlabel('number of features used')
+    plt.xticks(x)
+    plt.title(f'{named}: Best features as determined by DecisionTree')
+    plt.legend()
+    
+
 five_best_features = X[['histogram_mean', 'mean_value_of_short_term_variability', 'percentage_of_time_with_abnormal_long_term_variability', 
     'abnormal_short_term_variability', 'accelerations']]
 
@@ -327,7 +360,7 @@ model_tests(tenfeat_train, tenfeat_test, y_train, y_test)
 #FEATURE SELECTION USING RF
 
 ###
-RF = RandomForestClassifier()
+RF = RandomForestClassifier(random_state=33)
 rfc = RF.fit(X_train, y_train)
 
 dictionary = {}
@@ -366,6 +399,72 @@ model_tests(rf_10besttrain, rf_10besttest, y_train, y_test)
 
 #RFR top 10 better for KNN, GNB, similar for DTC except ROC and slightly loweer than top 10 from DTC importances
 
+models = {"KNeighborsClassifier":KNeighborsClassifier(), "GaussianNaiveBayes": GaussianNB(), "RandomForestClassifier": RandomForestClassifier(random_state=10)}
+for named, model in models.items(): 
+   
+    accuracyscores = []
+    precisionscores = []
+    recallscores= []
+    f1scores = []
+    
+    for i in range(len(dictionary.keys())):
+        feature_names = list(dictionary.keys())[:i+1]
+        best_X = X[feature_names]  
+
+        best_X_train, best_X_test, y_train, y_test = train_test_split(best_X, y, test_size=0.2, random_state = 40)
+        
+        model.fit(best_X_train, y_train)
+        ypred = model.predict(best_X_test)
+
+    #generating scores 
+        accuracyscores.append(accuracy_score(y_test, ypred))
+        precisionscores.append(precision_score(y_test, ypred, average = 'macro'))
+        recallscores.append(recall_score(y_test, ypred, average = 'macro'))
+        f1scores.append(f1_score(y_test, ypred, average="macro"))
+    
+    plt.figure()
+    x = range(1, len(dictionary.keys())+1)
+    plt.plot(x,accuracyscores, label = 'accuracy scores', color="green")
+    plt.plot(x,precisionscores, label = 'precision scores', color="blue")
+    plt.plot(x, recallscores, label = 'recall scores',color="red")
+    plt.plot(x, f1scores, label = 'F1 scores',color="purple")
+    plt.xlabel('number of features used')
+    plt.xticks(x)
+    plt.title(f'{named}: Best features as determined by RandomForest Importances')
+    plt.legend()
+
+
+
+# Agreed preprocessing step
+
+def preprocess (data):
+    data = data.copy()
+   
+    X = data[data.columns.to_list()[:-1]] #.............. #predicting this/target
+    y = data['fetal_health'] 
+
+
+    OS = RandomOverSampler(sampling_strategy={2:1655, 3:1655}, random_state=40)
+    X,y = OS.fit_resample(X,y)
+
+
+    main_data = X
+    targets = y
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state=10)
+    
+    #instantiate scaler
+    scaler = RobustScaler()
+    #fit training data of X
+    scaler.fit(X_train)
+    
+    X_train = pd.DataFrame (scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
+    X_test = pd.DataFrame (scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
+    
+    #we would return the split data so we can work on them
+    return  X_train, X_test, y_train, y_test
+
+X_train, X_test, y_train,y_test = preprocess(data)
 
 ## Hyperparameter tuning for KNN
 
@@ -377,11 +476,25 @@ p = [1,2]
 params_knn = dict(n_neighbors=n_neighbors, weights=weights, leaf_size=leaf_size, p=p)
 
 knn_grid_search = GridSearchCV(estimator = KNeighborsClassifier(), param_grid = params_knn, scoring = 'accuracy')
-knn_grid_search.fit(scaled_X_train, y_train)
+knn_grid_search.fit(X_train, y_train)
+knn_y_pred = knn_grid_search.predict(X_test)
 
 print(knn_grid_search.best_params_)
 print(f"\n >>> Results for optimised KNN")
-model_tests(scaled_X_train, scaled_X_test, y_train, knn_y_pred)
+
+accuracy = round((accuracy_score(y_test, knn_y_pred)),2)
+precision = round((precision_score(y_test, knn_y_pred, average="weighted")),2)
+recall = round((recall_score(y_test, knn_y_pred, average="weighted")),2)
+f1score = round((f1_score(y_test, knn_y_pred, average="weighted")),2)
+    
+print(f"Accuracy for {named}: {accuracy}")
+print(f"Weighted precision for {named}: {precision}")
+print(f"Weighted Recall for {named}: {recall}")
+print(f"Weighted F1 score for {named}: {f1score}")
+
+class_report = classification_report(y_test, ypred, labels=[1,2,3], target_names=["Normal", "Suspect", "Pathological"])
+print(f"Classification report by class for {named}")
+print(class_report)
 
 print("--------------------")
 #Hyperparameter tuning with GNB
@@ -401,17 +514,73 @@ print(gnb_grid_search.best_params_)
 best_gnb=GaussianNB(var_smoothing=1e-10)
 #fit the best parameter 
 best_gnb_model=best_gnb.fit(X_train, y_train)
-best_y_pred= best_gnb_model.predict(X_test)
+gnb_pred= best_gnb_model.predict(X_test)
 
-print(f"\n >>> Results for optimised GNB")
-model_tests(scaled_X_train, scaled_X_test, y_train, best_y_pred)
+print(f"\n -------------------\nResults for optimised GNB")
+accuracy = round((accuracy_score(y_test, gnb_pred)),2)
+precision = round((precision_score(y_test, gnb_pred, average="weighted")),2)
+recall = round((recall_score(y_test, gnb_pred, average="weighted")),2)
+f1score = round((f1_score(y_test, gnb_pred, average="weighted")),2)
+    
+print(f"Accuracy for {named}: {accuracy}")
+print(f"Weighted precision for {named}: {precision}")
+print(f"Weighted Recall for {named}: {recall}")
+print(f"Weighted F1 score for {named}: {f1score}")
 
-print(f"Accuracy for tuned GNB model : {accuracy_score (y_test, best_y_pred)}") 
-print(f"Precision for tuned GNB model: {precision_score (y_test, best_y_pred, average= 'micro')}")
-print(f"Recall for tuned GNB model : {recall_score(y_test, best_y_pred, average= 'micro')}")
+class_report = classification_report(y_test, ypred, labels=[1,2,3], target_names=["Normal", "Suspect", "Pathological"])
+print(f"Classification report by class for {named}")
+print(class_report)
+
+# print(f"Accuracy for tuned GNB model : {accuracy_score (y_test, best_y_pred)}") 
+# print(f"Precision for tuned GNB model: {precision_score (y_test, best_y_pred, average= 'micro')}")
+# print(f"Recall for tuned GNB model : {recall_score(y_test, best_y_pred, average= 'micro')}")
 
 #accuracy before 0.7541666666666667 vs after 0.7774420946626385
 #precision before 0.7541666666666667 vs after 0.0.77744209466263857
 #Recall  before 0.7541666666666667 vs after 0.7774420946626385
 
 #2% increase post tuning
+
+# RF hyperparm tuning
+rf = RandomForestClassifier(random_state=10)
+
+# use iterable parameters, else it gave me an error
+
+rf_params = {
+    'min_samples_split': [1,2,3,4],
+    'min_samples_leaf': [1,2,3],
+    'min_impurity_decrease': [0.0,0.1,0.2,0.3,0.4,0.5],
+    'max_features':["sqrt","log2"],
+    'n_estimators': [25, 50, 100],
+    'criterion': ["gini","entropy","log_loss"],
+    'max_samples':[0.5,1.0],
+    
+}
+
+rf_grid_search = GridSearchCV(estimator=rf, 
+                           param_grid=rf_params, 
+                           cv=3, n_jobs=-1, verbose=1, scoring = "accuracy")
+
+rf_grid_search.fit(X_train, y_train)
+print("Best RF parameters:", rf_grid_search.best_params_)
+
+rf_model= RandomForestClassifier(min_impurity_decrease = 0.0, min_samples_leaf = 1,random_state = 10, min_samples_split= 3)
+rf_model.fit(X_train, y_train)
+rf_pred = rf_model.predict(X_test)
+#pred_y
+ 
+print("----------\nRF final results")
+accuracy = round((accuracy_score(y_test, rf_pred)),2)
+precision = round((precision_score(y_test, rf_pred, average="weighted")),2)
+recall = round((recall_score(y_test, rf_pred, average="weighted")),2)
+f1score = round((f1_score(y_test, rf_pred, average="weighted")),2)
+    
+print(f"Accuracy for {named}: {accuracy}")
+print(f"Weighted precision for {named}: {precision}")
+print(f"Weighted Recall for {named}: {recall}")
+print(f"Weighted F1 score for {named}: {f1score}")
+
+class_report = classification_report(y_test, ypred, labels=[1,2,3], target_names=["Normal", "Suspect", "Pathological"])
+print(f"Classification report by class for {named}")
+print(class_report)
+
